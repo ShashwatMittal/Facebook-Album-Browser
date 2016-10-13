a42f8631fd1d04fabfa626e7cee147606b2eec96
Shashwat Mittal <shashwat.mittal5@gmail.com>
2016-10-13 21:05:22 +0530

    Handled the callback URL protocol. Saving the access token received in JAvascript into PHP session.

---
+++ callback.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+
+//Include Facebook PHP SDK
+require_once __DIR__ . '/Facebook/autoload.php';
+
+//Include the App ID, App Secret and the Callback url.
+require_once 'config.php';
+
+// JS Helper Returns the Access Token and the Facebook CLient stored in the 
+$jsHelper		 = $fb->getJavaScriptHelper();
+// @TODO This is going away soon
+$facebookClient	 = $fb->getClient();
+
+try {
+	$accessToken = $jsHelper->getAccessToken( $facebookClient );
+} catch ( Facebook\Exceptions\FacebookResponseException $e ) {
+	// When Graph returns an error
+	echo 'Graph returned an error: ' . $e->getMessage();
+} catch ( Facebook\Exceptions\FacebookSDKException $e ) {
+	// When validation fails or other local issues
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+}
+
+if ( isset( $accessToken ) ) {
+	// Logged in.
+} else {
+	// Unable to read JavaScript SDK cookie
+}
+
+if ( !isset( $accessToken ) ) {
+	if ( $helper->getError() ) {
+		header( 'HTTP/1.0 401 Unauthorized' );
+		echo "Error: " . $helper->getError() . "\n";
+		echo "Error Code: " . $helper->getErrorCode() . "\n";
+		echo "Error Reason: " . $helper->getErrorReason() . "\n";
+		echo "Error Description: " . $helper->getErrorDescription() . "\n";
+	} else {
+		header( 'HTTP/1.0 400 Bad Request' );
+		echo 'Bad request';
+	}
+	exit;
+}
+
+// Logged in
+$_SESSION[ 'accessToken' ] = $accessToken->getValue();
+
+// The OAuth 2.0 client handler helps us manage access tokens
+$oAuth2Client = $fb->getOAuth2Client();
+
+// Get the access token metadata from /debug_token
+$tokenMetadata = $oAuth2Client->debugToken( $accessToken );
+
+// Validation (these will throw FacebookSDKException's when they fail)
+$tokenMetadata->validateAppId( '513040895560036' ); // Replace {app-id} with your app id
+// If you know the user ID this access token belongs to, you can validate it here
+//$tokenMetadata->validateUserId('123');
+$tokenMetadata->validateExpiration();
+
+
+if ( !$accessToken->isLongLived() ) {
+	// Exchanges a short-lived access token for a long-lived one
+	try {
+		$accessToken = $oAuth2Client->getLongLivedAccessToken( $accessToken );
+	} catch ( Facebook\Exceptions\FacebookSDKException $e ) {
+		echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+		exit;
+	}
+}
+
+
+// User is logged in with a long-lived access token.
+// You can redirect them to a members-only page.
+header( 'Location: http://fb.shashwatmittal.com/home.php' );
+?>
+
+<!--To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.-->
