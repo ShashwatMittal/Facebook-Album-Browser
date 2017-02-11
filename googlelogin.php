4a31283d596efd53cb7b3b7b1a88a71a4841543e
Shashwat Mittal <shashwat.mittal5@gmail.com>
2017-02-12 00:03:14 +0530

    Google Invalid Token error

--- googlelogin.php
+++ googlelogin.php
@@ -4,37 +4,77 @@ require_once __DIR__ . '/vendor/autoload.php';
 session_start();
 
 
-$client		 = new Google_Client();
-$client->setAuthConfig( 'client_id.json' ); // Client Credentials. Sent to Google for Authentication.
-$client->addScope( 'https://picasaweb.google.com/data/' ); // Scope or Permissions requested from the User.
-$auth_url	 = $client->createAuthUrl();
+$client = new Google_Client();
+//$client->setApplicationName( 'Facebook Album Browser' );
+//$client->setDeveloperKey( 'AIzaSyAuXZ6aTJY111jAksoUoqXG7mYjiwVOUh4' );
+$client->setAuthConfig( 'client_secret.json' );
+$client->addScope( 'https://picasaweb.google.com/data/' );
+$client->setAccessType( 'offline' );
+//$auth_url	 = $client->createAuthUrl();
 
 if ( isset( $_SESSION[ 'access_token' ] ) && $_SESSION[ 'access_token' ] ) {
-	$access_token	 = $_SESSION[ 'access_token' ];
+	//Refreshing the Access Token using the Refresh token recieved when the user authorizes the request.
+	if ( $client->isAccessTokenExpired() ) {
+		$client->revokeToken();
+		$_SESSION[ 'access_token' ] = $client->refreshToken( '1/_k6Sk3rOPpCjSaHczgi-3i4xoeXxLWf59519N7qfQ58' );
+	}
+
 	$client->setAccessToken( $_SESSION[ 'access_token' ] );
-	$url			 = 'https://picasaweb.google.com/data/feed/api/user/default';
+	$data		 = array( 'access_token' => $_SESSION[ 'access_token' ][ 'access_token' ] );
+	$response	 = file_get_contents( 'https://picasaweb.google.com/data/feed/api/user/default' . '?' . http_build_query( $data ) );
+	$result		 = simplexml_load_string( $response );
 
-	$queryData = array( 'access_token' => $access_token[ 'access_token' ], 'v' => '2' );
-
-	$query = http_build_query( $queryData );
-
-	$query_url = $url . $query;
-
-	$data		 = file_get_contents( $query_url );
-	var_dump( $data );
-	$response	 = simplexml_load_string( $data );
-	$entries	 = $response->entry; // XML Feed returned from Google's Servers consisting of all the links to the User photo albums.
-	$albumNames	 = array();
-	// Iterating over the response.
-	foreach ( $entries as $entry ) {
+	foreach ( $result->entry as $entry ) {
 		echo "<pre>";
-		array_push( $albumNames, (string) $entry->title );
-		print_r( $albumNames ); // Each entry represents the Data related to an Album in Picasa Web Album.
+		$albumData = getAlbumLink( $entry );
+		print_r( $albumData );
 		echo "</pre>";
 	}
 } else {
-	$redirect_uri = 'http://' . $_SERVER[ 'HTTP_HOST' ] . '/oauth2callback.php'; // Redirecting to Oauth CallBack.
+	$redirect_uri = 'https://' . $_SERVER[ 'HTTP_HOST' ] . '/oauth2callback.php';
 	header( 'Location: ' . filter_var( $redirect_uri, FILTER_SANITIZE_URL ) );
 }
 
 
+function getAlbumLink( $entry ) {
+	$attributes	 = (array) $entry->link;
+	$link		 = $attributes[ '@attributes' ][ 'href' ];
+	echo "<pre>";
+	uploadPhoto( $link );
+//	print_r( $link );
+	echo "</pre>";
+}
+
+// Converts the image data into binary and creates a CURL request and sends the post request to the servers.
+function uploadPhoto( $link ) {
+	$filename	 = __DIR__ . '/lib/images/image1.jpeg';
+	$fileSize	 = filesize( $filename );
+	$fh			 = fopen( $filename, 'rb' );
+	$imgData	 = fread( $fh, $fileSize );
+	fclose( $fh );
+	$header		 = array('GData-Version: 3', 'Authorization: GoogleLogin auth= Bearer ' . $_SESSION[ 'access_token' ], 'Content-Type: image/jpeg', 'Content-Length: ' . $fileSize, 'Slug: ' . basename( $filename ) );
+	echo "<pre>";
+	print_r( curl_post( $link, $header, $imgData ) );
+	echo "</pre>";
+//	$context		 = stream_context_create( array( http => array( 'method' => 'POST', 'header' => 'Content-Type: image/jpeg\r\n' . 'Content-Length: ' . $imgDataLength . '\r\n' . 'Slug: ' . basename( $filename ) . '\r\n\r\n' . $imageData ) ) );
+//	var_dump($response);exit;
+//	echo "</pre>";
+//	return $response;
+}
+
+function curl_post( $link, $header, $data ) {
+	$ch		 = curl_init();
+	$options = array(
+		CURLOPT_URL				 => $link,
+		CURLOPT_POST			 => TRUE,
+		CURLOPT_RETURNTRANSFER	 => TRUE,
+		CURLOPT_HEADER			 => TRUE,
+		CURLOPT_FOLLOWLOCATION	 => TRUE,
+		CURLOPT_POSTFIELDS		 => $data,
+		CURLOPT_HTTPHEADER		 => $header,
+	);
+	curl_setopt_array( $ch, $options );
+	$ret	 = curl_exec( $ch );
+	curl_close( $ch );
+	return $ret;
+}
--- oauth2callback.php
+++ oauth2callback.php
@@ -1,22 +1,20 @@
 <?php
+
 session_start();
 require_once __DIR__ . '/vendor/autoload.php';
 
 $client = new Google_Client();
-$client->setAuthConfigFile( 'client_id.json' );
+$client->setAuthConfigFile( 'client_secret.json' );
 $client->setRedirectUri( 'http://' . $_SERVER[ 'HTTP_HOST' ] . '/oauth2callback.php' );
 $client->addScope( 'https://picasaweb.google.com/data/' );
+$client->setAccessType('offline');
 
 if ( !isset( $_GET[ 'code' ] ) ) {
 	$auth_url = $client->createAuthUrl();
 	header( 'Location: ' . filter_var( $auth_url, FILTER_SANITIZE_URL ) );
 } else {
-	$token = $client->fetchAccessTokenWithAuthCode( $_GET[ 'code' ] );
-	echo "<pre>";
-	print_r( $token );
-	echo "</pre>";
-
 	$client->authenticate( $_GET[ 'code' ] );
+//	$client->fetchAccessTokenWithAuthCode( $_GET[ 'code' ] );
 	$_SESSION[ 'access_token' ]	 = $client->getAccessToken();
 	$redirect_uri				 = 'http://' . $_SERVER[ 'HTTP_HOST' ] . '/googlelogin.php';
 	header( 'Location: ' . filter_var( $redirect_uri, FILTER_SANITIZE_URL ) );
