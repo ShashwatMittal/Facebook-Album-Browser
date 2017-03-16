1af78afbf12686bf16053d1c656289dd4d29def7
Shashwat Mittal <shashwat.mittal5@gmail.com>
2017-03-16 18:23:20 +0530

    Deletes unnecessary files and fixes callback file.

--- callback.php
+++ callback.php
@@ -1,4 +1,16 @@
 <?php
+
+/**
+ * Uses the JS Helper class and gets the Facebook Client and Access Token 
+ * retrieved from the Facebook Graph API and stores these variables in session.
+ * 
+ * @category Authorisation
+ * @package  FBAlbumBrowser
+ * @author   Shashwat Mittal <shashwat.mittal5@gmail.com>
+ * @license  http://URL name
+ * @version  1.0.0
+ * @link     https://fb.shashwatmittal.com/callback.php
+ */
 session_start();
 
 //Include Facebook PHP SDK
@@ -8,38 +20,33 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once 'config.php';
 
 // JS Helper Returns the Access Token and the Facebook CLient stored in the 
-$jsHelper		 = $fb->getJavaScriptHelper();
+$jsHelper       = $fb->getJavaScriptHelper();
 // @TODO This is going away soon
-$facebookClient	 = $fb->getClient();
+$facebookClient = $fb->getClient();
 
 try {
-	$accessToken = $jsHelper->getAccessToken( $facebookClient );
+    $accessToken = $jsHelper->getAccessToken( $facebookClient );
 } catch ( Facebook\Exceptions\FacebookResponseException $e ) {
-	// When Graph returns an error
-	echo 'Graph returned an error: ' . $e->getMessage();
+    // When Graph returns an error
+    header( 'Location: http://fb.shashwatmittal.com/index.php' );
+//    echo 'Graph returned an error: ' . $e->getMessage();
 } catch ( Facebook\Exceptions\FacebookSDKException $e ) {
-	// When validation fails or other local issues
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-}
-
-if ( isset( $accessToken ) ) {
-	// Logged in.
-} else {
-	// Unable to read JavaScript SDK cookie
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
 }
 
 if ( !isset( $accessToken ) ) {
-	if ( $helper->getError() ) {
-		header( 'HTTP/1.0 401 Unauthorized' );
-		echo "Error: " . $helper->getError() . "\n";
-		echo "Error Code: " . $helper->getErrorCode() . "\n";
-		echo "Error Reason: " . $helper->getErrorReason() . "\n";
-		echo "Error Description: " . $helper->getErrorDescription() . "\n";
-	} else {
-		header( 'HTTP/1.0 400 Bad Request' );
-		echo 'Bad request';
-	}
-	exit;
+    if ( $helper->getError() ) {
+        header( 'HTTP/1.0 401 Unauthorized' );
+        echo "Error: " . $helper->getError() . "\n";
+        echo "Error Code: " . $helper->getErrorCode() . "\n";
+        echo "Error Reason: " . $helper->getErrorReason() . "\n";
+        echo "Error Description: " . $helper->getErrorDescription() . "\n";
+    } else {
+        header( 'HTTP/1.0 400 Bad Request' );
+        echo 'Bad request';
+    }
+    exit;
 }
 
 // Logged in
@@ -59,21 +66,17 @@ $tokenMetadata->validateExpiration();
 
 
 if ( !$accessToken->isLongLived() ) {
-	// Exchanges a short-lived access token for a long-lived one
-	try {
-		$accessToken = $oAuth2Client->getLongLivedAccessToken( $accessToken );
-	} catch ( Facebook\Exceptions\FacebookSDKException $e ) {
-		echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
-		exit;
-	}
+    // Exchanges a short-lived access token for a long-lived one
+    try {
+        $accessToken = $oAuth2Client->getLongLivedAccessToken( $accessToken );
+    } catch ( Facebook\Exceptions\FacebookSDKException $e ) {
+        echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+        exit;
+    }
 }
 
 
 // User is logged in with a long-lived access token.
 // You can redirect them to a members-only page.
 header( 'Location: http://fb.shashwatmittal.com/home.php' );
-?>
-
-<!--To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.-->
+exit;
