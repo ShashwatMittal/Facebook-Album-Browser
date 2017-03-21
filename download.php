46fc5ca0599fe0e22f50f669792cd26f9d349ae4
Shashwat Mittal <shashwat.mittal5@gmail.com>
2017-03-21 18:12:12 +0530

    Updates file locations.

--- download.php
+++ download.php
@@ -20,6 +20,10 @@ require_once 'functions.php';
 
 $accessToken = $_SESSION[ 'accessToken' ];
 
+$userID = getUserData($fb, $accessToken);
+
+$zipPath = '/var/www/albums-'.userHash($userID).'.zip';
+
 /**
  * Donwloads the Photos on the server and saves them inside a folder named as the Album Name.
  */
@@ -30,26 +34,29 @@ if (isset($_GET[ 'albums' ]) ) {
         foreach ( $albums as $album ) {
             $data = getPhotosForAlbumId($album[ 'id' ], $fb, $accessToken);
             foreach ( $data as $url ) {
-                $image = downloadImage($url, 'images/' . trim($album[ 'name' ], ' '));
+                $albumPath = 'images-' . userHash( $userID ) . '/' . trim( $album[ 'name' ] );
+                //Download and store image in the folder
+                downloadImage( $url, $albumPath );
             }
         }
     }
 }
 
-//This zips all the albums downloaded on the server.
+
+//Download is finished, This zips all the albums downloaded on the server.
 if (isset($_GET[ 'zip' ]) ) {
-    zip($_SERVER[ 'DOCUMENT_ROOT' ] . '/' . $_GET[ 'zip' ], 'albums.zip');
+    zip($_SERVER[ 'DOCUMENT_ROOT' ] . '/' . 'images-'.userHash($userID), $zipPath);
 }
 
 // Deletes the images once they are zipped and ready to be downloaded.
 if (isset($_GET[ 'deleteImages' ]) ) {
-    deletePath($_GET[ 'deleteImages' ]);
+    deletePath('images-'.  userHash( $userID).'');
     echo 'Images Deleted.';
 }
 
 // Deletes the zip file if the Modal is closed.
 if (isset($_GET[ 'deleteZip' ]) ) {
-    deletePath($_GET[ 'deleteZip' ]);
+    deletePath( $zipPath );
     echo 'Zip file deleted.';
 }
 
